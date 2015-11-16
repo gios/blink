fef02945cf5abf3cfd2741bd6de34b6a165d1751
gios <pblazhchuk@gmail.com>
2015-11-16 17:47:06 +0200

    Deploy option

---
+++ webpack.production.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
