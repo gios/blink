7296c0cbde8dcea45dfce5559d3aa19a1994100c
gios <pblazhchuk@gmail.com>
2015-11-17 11:05:35 +0200

    Generate angular folder structure

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('blink')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
